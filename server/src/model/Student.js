b4bb5407062a7262bef4865cd3b6dc44843c5bca
ArnavSharma005 <arnavoct.20@gmail.com>
2024-08-10 12:08:16 +0530

    student schema added

---
+++ server/src/model/Student.js
@@ -0,0 +1,44 @@
+const mongoose=require("mongoose");
+const StudentSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    roll:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    hostel:{
+        type:String,
+        required:true
+    },
+    room:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    gender:{
+    type: String,
+    enum: ['male', 'female', 'others'],
+    default:'male'
+    },
+    issue:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Issue",
+        required:true
+    }]    
+})
+
+const Student=mongoose.model("Student",StudentSchema);
+module.exports=Student;
