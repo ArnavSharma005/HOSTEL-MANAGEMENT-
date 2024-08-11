1101c7159efe397363fd05888d0c97b1f94f4f06
sks.bholi <sks.bholi@gmail.com>
2024-08-11 16:50:31 +0530

    Added worker schema and login feature

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import studentModel from "../model/Student.js"
 import adminModel from "../model/Admin.js"
+import workerModel from "../model/Worker.js"
 import bcrypt from "bcryptjs"
 //signup student
 
@@ -38,26 +39,7 @@ export const signupStudent = async (req, res) => {
     res.status(500).json({ msg: "Server Error" })
   }
 }
-//student login
-export const loginStudent = async (req, res) => {
-  const { roll, password } = req.body
-  //checking if user exist or not
-  try {
-    const student = await studentModel.findOne({ roll })
-    if (!student) {
-      return res.status(400).json({ msg: "User not found", error: true })
-    }
-    //comparing password
-    const isMatch = await bcrypt.compare(password, student.password)
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid password", error: true })
-    }
-    res.json({ msg: "Login successful", data: student, error: false })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ msg: "Server Error" })
-  }
-}
+
 //admin signup
 
 export const signupAdmin = async (req, res) => {
@@ -89,21 +71,92 @@ export const signupAdmin = async (req, res) => {
   }
 }
 
-//admin login
-export const loginAdmin = async (req, res) => {
+//worker signup
+
+export const signupWorker = async (req, res) => {
+  const { name, email, mobile, password, role } = req.body
+  //checking if admin already exist or not
+  try {
+    const existingWorker = await workerModel.findOne({ email })
+    if (existingWorker) {
+      return res.status(400).json({ msg: "Worker already exists", error: true })
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newWorker = new workerModel({
+      name,
+      email,
+      mobile,
+      password: hashedPassword,
+      role,
+    })
+    const savedWorker = await newWorker.save()
+
+    res.json({
+      msg: "worker registered successfully",
+      data: savedWorker,
+      error: false,
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ msg: "Server Error" })
+  }
+}
+
+// login
+export const login = async (req, res) => {
   const { email, password } = req.body
   //checking if user exist or not
   try {
-    const admin = await adminModel.findOne({ email })
+    let worker, admin, student
+    admin = await adminModel.findOne({ email })
     if (!admin) {
+      worker = await workerModel.findOne({ email })
+    }
+    if (!admin && !worker) {
+      if (/^\d+$/.test(email)) {
+        student = await studentModel.findOne({ roll: email })
+      } else {
+        student = await studentModel.findOne({ email })
+      }
+    }
+
+    if (!admin && !student && !worker) {
+      //checking if exist in student
+
       return res.status(400).json({ msg: "User not found", error: true })
     }
     //comparing password
-    const isMatch = await bcrypt.compare(password, admin.password)
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid password", error: true })
+    if (admin) {
+      const isMatch = await bcrypt.compare(password, admin.password)
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid password", error: true })
+      }
+      res.json({
+        msg: "Login successful",
+        data: { userData: admin, role: "admin" },
+        error: false,
+      })
+    } else if (student) {
+      const isMatch = await bcrypt.compare(password, student.password)
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid password", error: true })
+      }
+      res.json({
+        msg: "Login successful",
+        data: { userData: student, role: "student" },
+        error: false,
+      })
+    } else if (worker) {
+      const isMatch = await bcrypt.compare(password, worker.password)
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid password", error: true })
+      }
+      res.json({
+        msg: "Login successful",
+        data: { userData: worker, role: "worker" },
+        error: false,
+      })
     }
-    res.json({ msg: "Login successful", data: admin, error: false })
   } catch (error) {
     console.error(error)
     res.status(500).json({ msg: "Server Error" })
